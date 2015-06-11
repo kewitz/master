985cdeb4aaadf6a6a4c548aa8a6bf07e72ebf780
Leonardo K. Kewitz <holland9@gmail.com>
2015-06-11 16:21:13 -0300

    Cálculo de erro no test_nodemap.

--- test_nodemap.py
+++ test_nodemap.py
@@ -13,7 +13,7 @@ path = """./res/"""
 split = ns.split
 m = 1
 
-print "DOF\tCUDA\tTime\tIterations"
+print "DOF\tCUDA\tTime\t\tIterations\tErr."
 for c in [(c, f) for f in ['teste1_1.msh', 'teste1_2.msh', 'teste1_3.msh', 'teste1_4.msh', 'teste1_5.msh'] for c in [False, True]]:
     del m
     cuda, fi = c
@@ -22,6 +22,14 @@ for c in [(c, f) for f in ['teste1_1.msh', 'teste1_2.msh', 'teste1_3.msh', 'test
     
     for n in m.nodesOnLine([1, 2, 3, 4]):
         n.calc = False
+    
+    if cuda:
+        vc = v
 
-    v, i, b = m.run(cuda=cuda, boundary=bound, R=0, errmin=1E-6)
-    print "%i\t%s\t%fs\t%i" % (len(m.nodes), cuda, b[0], i)
+    v, i, b = m.run(cuda=cuda, boundary=bound, R=0, errmin=1E-6, kmax=1000000)
+
+    e = 0.0
+    if cuda:
+        e = max(abs(vc - v))
+
+    print "%i\t%s\t%fs\t%i\t%f" % (len(m.nodes), cuda, b[0], i, e)
